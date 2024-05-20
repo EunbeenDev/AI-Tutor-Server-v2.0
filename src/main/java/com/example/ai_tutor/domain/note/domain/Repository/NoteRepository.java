0632dd05da33e6a94ea7a1e9fae8414cd1c66dc8
EunbeenDev <ros4312@naver.com>
2024-05-21 01:57:32 +0900

    [FEAT] 노트 생성 및 노트 리스트 조회 기능 구현
    노트별 학습 단계: 0~4

---
+++ src/main/java/com/example/ai_tutor/domain/note/domain/Repository/NoteRepository.java
@@ -0,0 +1,13 @@
+package com.example.ai_tutor.domain.note.domain.Repository;
+
+import com.example.ai_tutor.domain.Folder.domain.Folder;
+import com.example.ai_tutor.domain.note.domain.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findAllByFolder(Folder folder);
+}
