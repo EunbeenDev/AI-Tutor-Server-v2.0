d96c64de7406c7f1fee95cb069ebf6874927bbaf
EunbeenDev <ros4312@naver.com>
2024-06-05 13:00:30 +0900

    [FEAT] AI 서버 연결 설정 및 노트 생성 요청 로직 수정

--- src/main/java/com/example/ai_tutor/AiTutorApplication.java
+++ src/main/java/com/example/ai_tutor/AiTutorApplication.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @PropertySource(value = { "classpath:oauth2/application-oauth2.yml" }, factory = YamlPropertySourceFactory.class)
 @PropertySource(value = { "classpath:database/application-database.yml" }, factory = YamlPropertySourceFactory.class)
 @PropertySource(value = { "classpath:s3/application-s3.yml" }, factory = YamlPropertySourceFactory.class)
+@PropertySource(value = { "classpath:webclient/application-webclient.yml" }, factory = YamlPropertySourceFactory.class)
 public class AiTutorApplication {
 
     public static void main(String[] args) {
